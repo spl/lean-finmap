67b767bf7fd70b60a45810ebddbc98358f67981f
Sean Leather <sean.leather@gmail.com>
2018-07-05 14:09:22 +0200

    multiset: keys, nodup_keys_iff

--- src/data/multiset/dict.lean
+++ src/data/multiset/dict.lean
@@ -15,6 +15,13 @@ quot.induction_on s $ λ _, nodup_of_nodup_keys
 
 @[simp] theorem nodup_keys_zero : @nodup_keys α β 0 := pairwise.nil _
 
+def keys [decidable_eq α] : multiset (sigma β) → multiset α :=
+map sigma.fst
+
+theorem nodup_keys_iff [decidable_eq α] {s : multiset (sigma β)} :
+  s.keys.nodup ↔ s.nodup_keys :=
+quot.induction_on s $ λ _, list.nodup_keys_iff
+
 def klookup [decidable_eq α] (a : α) (s : multiset (sigma β)) : multiset (β a) :=
 quot.lift_on s
   (λ l, (l.klookup_all a : multiset (β a)))
